756ad181c47b9cfcce67b12150d1a4f440b6aa22
Victor Xavier <victorxm1@hotmail.com>
2017-03-07 11:16:03 -0300

    exercicio2

---
+++ pratica1/exercicio2/MeuPrograma.java
@@ -0,0 +1,9 @@
+// Victor Xavier de Melo  15.01027-9
+class MeuPrograma {
+	public static void main (String[] args) {
+	
+		//Mensagem de saída do sistema
+		System.out.println("Minha primeira aplicação Java!");
+		
+		}
+		}
