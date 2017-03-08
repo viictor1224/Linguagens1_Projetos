05189681f603daf6fa0f58347f469e78410d62d5
Victor Xavier <victorxm1@hotmail.com>
2017-03-08 10:17:58 -0300

    ex3

---
+++ pratica1/exercicio3/MeuPrograma.java
@@ -0,0 +1,10 @@
+// Victor Xavier de Melo  15.01027-9
+class MeuPrograma {
+	public static void main (String[] args) {
+
+		//Mensagem de saída do sistema
+		System.out.println("Olá Mundo!");
+		System.out.println("Estou programando no modo Conan. :");
+
+		}
+		}
