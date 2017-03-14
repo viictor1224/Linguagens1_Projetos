5caafc438712cdecfc29733f0b9a9545c8558043
Victor Xavier <victorxm1@hotmail.com>
2017-03-14 11:05:11 -0300

    ex4

--- pratica1/exercicio4/Aula03/src/aula03/Aula03.java
+++ pratica1/exercicio4/Aula03/src/aula03/Aula03.java
@@ -1,25 +1,28 @@
-
 package aula03;
 
 public class Aula03 {
 
     public static void main(String[] args) {
-                     
+
         Conta c1 = new Conta();
+        c1.titular = new Cliente();
+        c1.titular.nome = "Victor";
+        c1.titular.sobrenome = "Xavier";
+        c1.titular.cpf = "12345678999";
+        c1.titular.visualizarCliente ();
+
+        //Conta c2 = new Conta();
+        //c2.saldo = 6500;
+        //c2.visualizarSaldo();
         c1.saldo = 1000;
         c1.visualizarSaldo();
-        
         c1.depositar(200);
         c1.visualizarSaldo();
-        
         c1.sacar(600);
         c1.visualizarSaldo();
-        
-//        Conta c2 = new Conta();
- //       c2.saldo = 6500;
-   //     c2.visualizarSaldo();
-        
-        
+        //c2.transferirPara(c1, 200);
+        //c1.visualizarSaldo();
+
     }
-    
+
 }
--- pratica1/exercicio4/Aula03/src/aula03/Conta.java
+++ pratica1/exercicio4/Aula03/src/aula03/Conta.java
@@ -1,30 +1,40 @@
-
 package aula03;
 
 public class Conta {
+
     int numero;
-    String titular;
-    String cpf;
     double saldo;
-    
-    void visualizarSaldo (){
+    Cliente titular;
+
+    void visualizarSaldo() {
         System.out.println("Saldo = " + this.saldo);
     }
-    void depositar (double valor ){
-        if (valor > 0){
-        this.saldo = this.saldo + valor;
-    }
-    }
-    void sacar (double valor){
-        
-        if (valor <= this.saldo){
-        this.saldo = this.saldo - valor;
-        }else{
-            System.out.println("Quer um empréstimo?");
+
+    void depositar(double valor) {
+        if (valor > 0) {
+            this.saldo = this.saldo + valor;
         }
     }
-    
-    void transferirDinheiro (){
-    
+
+    boolean sacar(double valor) {
+
+        if (valor <= this.saldo) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean transferirPara(Conta c1, double valor) {
+
+        if (this.sacar(valor)) {
+            c1.depositar(valor);
+            System.out.println("Transferencia efetuada.");
+            return true;
+        } else {
+            System.out.println("Transferencia nao efetuada");
+            return false;
+        }
     }
 }
