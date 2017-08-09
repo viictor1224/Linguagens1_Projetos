724459d536b4bdf9c9c6ae79d8c4971694c441a2
Victor Xavier <victorxm1@hotmail.com>
2017-08-09 10:51:00 -0300

    calc

---
+++ pratica5/telaConan/src/telaconan/TelaConan.java
@@ -0,0 +1,22 @@
+
+
+
+package telaconan;
+
+import javax.swing.JFrame;
+
+
+
+ 
+public class TelaConan {
+
+   
+    
+    public static void main(String[] args) {
+       
+        TelaPrincipal telaP = new TelaPrincipal();
+        telaP.setVisible(true);
+        
+    }
+    
+}
