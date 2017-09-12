b83561ae5ecc8f64cea0763eacfc733b1545c7cb
Victor Xavier <victorxm1@hotmail.com>
2017-09-12 11:07:58 -0300

    pkgs

---
+++ pratica10/OrganizandoJavaV2/src/br/com/xavier/organizando/Cliente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.xavier.organizando;
+
+public class Cliente {
+
+    public String nome;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String retornaNome() {
+        return nome;
+    }
+}
---
+++ pratica10/OrganizandoJavaV2/src/organizandojavav2/OrganizandoJavaV2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package organizandojavav2;
+
+import br.com.pacote2.organizando.Cliente;
+
+/**
+ *
+ * @author Aluno 14
+ */
+public class OrganizandoJavaV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    br.com.pacote2.organizando.Cliente eu = new br.com.pacote2.organizando.Cliente();
+    
+        
+    }
+    
+    }
+    
+}
